de9788f2e956e2690577bdac0a22c2db8254327d
Nikolai <59785043+nikolai3ldwin@users.noreply.github.com>
2021-12-18 03:02:38 -0700

    Fixed Home Style
    Turned the Home js file into a style js file. Fixed how the background looks and the PDL fixed box was on the home -- left it on the Gaming child page. Fixed the background tile repeating problem. Fixed the link color problem. Fixed all of the funky gaps. Cleaned up the code a bit. Added a Consulting tab to the nav bar.

--- Project Dragons Lair Gaming.js
+++ Project Dragons Lair Gaming.js
@@ -1,17 +1,3 @@
-/* The following code is for the Responsive Top Navigation */
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-/* End of code for the Responsive Top Navigation */
-
 /* The following code is for the slideshow */
 
 var slideIndex = 1;
